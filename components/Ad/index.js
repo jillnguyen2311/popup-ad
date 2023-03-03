4717edcf3ce3e9baa57d4002b38c7d5bc359ede4
jillnguyen2311 <113265779+jillnguyen2311@users.noreply.github.com>
2023-03-02 17:01:25 -0800

    Update index.js

--- components/Ad/index.js
+++ components/Ad/index.js
@@ -20,3 +20,30 @@ export default function Ad(){
         </>
     )
 }
+
+/*
+import { useState } from "react";
+import styles from './Ad.module.css'
+
+export default function Ad(){
+
+    const [popup, setPopup] = useState(false);
+
+    return(
+        <>
+        
+            <Image onClick={setPopup(true)}/>
+            {
+                popup ?
+                    <div className={styles.container}>
+                        <div className={styles.popup__area}> 
+                            <div onClick={() => setPopup(false)}>X</div>
+                            welcome
+                        </div>
+                    </div>
+                    : <></>
+            }
+        </>
+    )
+}
+*/
